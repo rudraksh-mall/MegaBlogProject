65eeb87feb1c4bcbd2e794925f8a09aca360cbc4
= <rudrakshmall553@gmail.com>
2025-06-20 00:30:26 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   return (
     <>
